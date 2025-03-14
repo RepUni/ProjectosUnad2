ddf7a11e5cb066ca2b781e365cbb9f44dfd945de
RepUni <2018-0327@unad.edu.do>
2025-03-13 23:00:42 -0400

    ejer10

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace ejer9
+namespace ejer10
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,57 +9,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ejer9
+namespace ejer10
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double ca = 0;
-        private double cd = 0;
-        private double acumuladas = 0;
-        private double acumuladasapro = 0;
-        private double acumuladasdesapro = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(txtNota.Text, out double nota))
+            if (int.TryParse(txtNumero.Text, out int numero))
             {
-                if (nota <= 10.5 && nota >= 0)
-                {
-                    cd++;
-                    acumuladasdesapro += nota;
-                }
-                else if (nota > 10.5 && nota <= 20)
-                {
-                    ca++;
-                    acumuladasapro += nota;
-                }
-                acumuladas += nota;
+                int somma = 0;
+                int residuo;
 
-                if (!chkOtraNota.IsChecked.Value)
+                do
                 {
-                    double promedioaprobadas = ca > 0 ? Math.Round(acumuladasapro / ca, 1) : 0;
-                    double promediodesaprobadas = cd > 0 ? Math.Round(acumuladasdesapro / cd, 1) : 0;
-                    double promedio = (ca + cd) > 0 ? Math.Round(acumuladas / (ca + cd), 1) : 0;
+                    residuo = numero % 10;
+                    somma += residuo;
+                    numero /= 10;
+                } while (numero != 0);
 
-                    lblDesaprobadas.Content = $"Cantidad de notas desaprobadas: {cd}";
-                    lblAprobadas.Content = $"Cantidad de notas aprobadas: {ca}";
-                    lblPromedioAprobadas.Content = $"Promedio de notas aprobadas: {promedioaprobadas}";
-                    lblPromedioDesaprobadas.Content = $"Promedio de notas desaprobadas: {promediodesaprobadas}";
-                    lblPromedioFinal.Content = $"Promedio final: {promedio}";
-                }
-                txtNota.Text = "";
+                lblSomma.Content = $"La somma delle cifre è: {somma}";
             }
             else
             {
-                MessageBox.Show("Por favor, introduce una nota válida.");
+                lblSomma.Content = "Inserisci un numero valido.";
             }
         }
-
     }
 }
