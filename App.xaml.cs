4366f2a7e10e4deacdaeb429826e917ad9f1908e
RepUni <2018-0327@unad.edu.do>
2025-03-13 23:05:27 -0400

    ejer19

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace ejer18
+namespace ejer19
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.VisualBasic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,7 +10,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ejer18
+namespace ejer19
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -19,6 +20,44 @@ namespace ejer18
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void IniciarProcesos_Click(object sender, RoutedEventArgs e)
+        {
+            double num1, num2, c, suma, resta, multiplicacion, division;
+            c = 0;
+            string output = "";
+
+            do
+            {
+                c = c + 1;
+                output += $"PROCESO No{c}:\n";
+
+                string num1Input = Interaction.InputBox("ingrese primer numero: ", $"PROCESO No{c}");
+                string num2Input = Interaction.InputBox("ingrese segundo numero: ", $"PROCESO No{c}");
+
+                if (double.TryParse(num1Input, out num1) && double.TryParse(num2Input, out num2))
+                {
+                    suma = num1 + num2;
+                    resta = num1 - num2;
+                    multiplicacion = num1 * num2;
+                    division = Math.Round(num1 / num2, 2);
+
+                    output += $"\nLa suma es: {suma}\n";
+                    output += $"La resta es : {resta}\n";
+                    output += $"La multiplicacion es: {multiplicacion}\n";
+                    output += $"La division es: {division}\n\n";
+                }
+                else
+                {
+                    output += "Entrada inválida. Proceso cancelado.\n\n";
+                    break;
+                }
+
+            }
+            while (c <= 9);
+
+            output += "final del proceso";
+            ResultadoTextBox.Text = output;
         }
     }
 }
