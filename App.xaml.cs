938509268be60860ed5606fc194b9972797c43f0
RepUni <2018-0327@unad.edu.do>
2025-03-13 23:02:27 -0400

    ejer13

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace ejer12
+namespace ejer13
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Text;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ejer12
+namespace ejer13
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,26 +21,23 @@ namespace ejer12
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CalcularFactorial_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(txtPrecio.Text, NumberStyles.Currency, CultureInfo.CurrentCulture, out double precio) &&
-                double.TryParse(txtArticulos.Text, out double numeroArticulos))
+            if (int.TryParse(NumeroTextBox.Text, out int numero))
             {
-                double precioVenta = precio * numeroArticulos;
-                double iva = Math.Round(precioVenta * 0.15, 2);
-                double precioBruto = precioVenta + iva;
-                double descuento = precioBruto >= 50 ? Math.Round((precioBruto * 5) / 100, 2) : 0;
-                double totalPagar = precioBruto - descuento;
+                int factorial = 1;
+                int i;
 
-                lblPrecioVenta.Content = $"Precio de venta: {precioVenta:C}";
-                lblIVA.Content = $"Impuesto sobre el valor añadido (IVA): {iva:C}";
-                lblPrecioBruto.Content = $"Precio bruto: {precioBruto:C}";
-                lblDescuento.Content = $"Descuento: {descuento:C}";
-                lblTotalPagar.Content = $"Total a pagar: {totalPagar:C}";
+                for (i = 1; i <= numero; i++)
+                {
+                    factorial = factorial * i; // factorial *= i
+                }
+
+                ResultadoTextBox.Text = $"{numero}! = {factorial}";
             }
             else
             {
-                MessageBox.Show("Por favor, ingrese valores numéricos válidos.");
+                MessageBox.Show("Por favor, ingrese un número entero válido.");
             }
         }
     }
